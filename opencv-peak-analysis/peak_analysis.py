3db57fc18f5b46a6b7b016071d70c2dccf8d4720
Nuha <nuha.shaikh@gmail.com>
2023-11-27 13:26:40 -0700

    plot all peak_analysis subplots at the same time

--- opencv-peak-analysis/peak_analysis.py
+++ opencv-peak-analysis/peak_analysis.py
@@ -4,7 +4,6 @@ from scipy.signal import find_peaks
 import argparse
 from scipy.fft import fft
 
-
 # Parse command line arguments
 parser = argparse.ArgumentParser(description='Perform peak detection and analyze red intensity over time.')
 parser.add_argument('input_file', type=str, help='Path to the .npy file containing red intensity over time data')
@@ -23,63 +22,52 @@ time = np.arange(0, num_frames) / fps
 # Perform peak detection
 peaks, _ = find_peaks(red_intensity_over_time, height=args.height, distance=args.distance)
 
-# Plot the detected peaks
-plt.plot(time, red_intensity_over_time, label='Red Intensity Over Time', color='red')
-plt.plot(time[peaks], red_intensity_over_time[peaks], 'x', label='Detected Peaks', color='blue')
-plt.xlabel('Time (seconds)')
-plt.ylabel('Red Intensity')
-plt.title('Detected Peaks in Red Intensity Over Time')
-plt.legend()
-plt.show()
-
 # Calculate time intervals between peaks
 peak_time_intervals = np.diff(time[peaks])
 
 # Extract HRV features using peak_time_intervals
-
-# Time-domain HRV features
 mean_rr_interval = np.mean(peak_time_intervals)
 std_rr_interval = np.std(peak_time_intervals)
 
-# Frequency-domain HRV features (you may need additional libraries for spectral analysis)
-# You can use methods like FFT to analyze the frequency content of the signal
 # Frequency-domain HRV features using FFT
-fft_result = fft(red_intensity_over_time) # fft is used to compute the FFT of the red intensity signal over time 
+fft_result = fft(red_intensity_over_time)
 frequencies = np.fft.fftfreq(len(fft_result), d=1/fps)
-# calculates the frequencies associated with the FFT result. It uses np.fft.fftfreq to
-# generate an array of frequencies based on the length of the FFT result (len(fft_result)) 
-# and the sampling rate (fps).
 positive_frequencies = frequencies[:len(frequencies)//2]
 fft_magnitude = np.abs(fft_result[:len(fft_result)//2])
-#  extracts the magnitude spectrum of the positive frequencies
-
-# Plot the FFT result
-# It visualizes how the magnitude of the signal varies across different frequencies.
-plt.figure()
-plt.plot(positive_frequencies, fft_magnitude)
-plt.xlabel('Frequency (Hz)')
-plt.ylabel('Magnitude')
-plt.title('Frequency Spectrum of Red Intensity Signal')
-plt.show()
 
 # Extract frequency-domain HRV features
-# You can define specific frequency bands (e.g., LF, HF) and calculate power in each band
-# Example: LF (low frequency) range [0.04, 0.15] Hz, HF (high frequency) range [0.15, 0.4] Hz
 lf_indices = np.where((positive_frequencies >= 0.04) & (positive_frequencies < 0.15))[0]
 hf_indices = np.where((positive_frequencies >= 0.15) & (positive_frequencies < 0.4))[0]
-
 lf_power = np.sum(fft_magnitude[lf_indices])
 hf_power = np.sum(fft_magnitude[hf_indices])
 
+# Plotting all figures in subplots
+fig, axes = plt.subplots(2, 2, figsize=(10, 8))
 
-# Display the calculated time intervals between peaks and HRV features
-print("Time Intervals between Peaks (seconds):", peak_time_intervals)
-print("Mean RR Interval:", mean_rr_interval)
-print("Standard Deviation of RR Intervals:", std_rr_interval)
+# Plot red intensity over time and detected peaks
+axes[0, 0].plot(time, red_intensity_over_time, label='Red Intensity Over Time', color='red')
+axes[0, 0].plot(time[peaks], red_intensity_over_time[peaks], 'x', label='Detected Peaks', color='blue')
+axes[0, 0].set_xlabel('Time (seconds)')
+axes[0, 0].set_ylabel('Red Intensity')
+axes[0, 0].set_title('Detected Peaks in Red Intensity Over Time')
+axes[0, 0].legend()
 
-# Visualize HRV features
-plt.figure()
-plt.bar(['Mean RR Interval', 'Standard Deviation of RR Intervals'], [mean_rr_interval, std_rr_interval], color=['blue', 'orange'])
-plt.ylabel('Time (sec)')
-plt.title('HRV Features')
+# Plot Frequency Spectrum of Red Intensity Signal
+axes[0, 1].plot(positive_frequencies, fft_magnitude)
+axes[0, 1].set_xlabel('Frequency (Hz)')
+axes[0, 1].set_ylabel('Magnitude')
+axes[0, 1].set_title('Frequency Spectrum of Red Intensity Signal')
+
+# Plot Histogram of Time Intervals between Peaks
+axes[1, 0].hist(peak_time_intervals, bins=30, color='green', alpha=0.7)
+axes[1, 0].set_xlabel('Time Intervals between Peaks (seconds)')
+axes[1, 0].set_ylabel('Frequency')
+axes[1, 0].set_title('Histogram of Time Intervals between Peaks')
+
+# Plot Bar Chart of HRV Features
+axes[1, 1].bar(['Mean RR Interval', 'Standard Deviation of RR Intervals'], [mean_rr_interval, std_rr_interval], color=['blue', 'orange'])
+axes[1, 1].set_ylabel('Time (sec)')
+axes[1, 1].set_title('HRV Features')
+
+plt.tight_layout()
 plt.show()
